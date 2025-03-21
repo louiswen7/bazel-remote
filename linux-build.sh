52867cbeb52745fef20062c00ddf5678271acccb
louiswen7 <louis_wen@foxmail.com>
2025-03-21 10:12:21 +0800

    Add server config yaml file

--- linux-build.sh
+++ linux-build.sh
@@ -10,4 +10,6 @@ GIT_TAGS=$(git tag --points-at HEAD | sort -h | paste -sd "," -)
 GIT_COMMIT_LINK_FLAG="main.gitCommit=${GIT_COMMIT}"
 GIT_TAGS_LINK_FLAG="main.gitTags=${GIT_TAGS}"
 
-CGO_ENABLED=1 GOOS=linux GOARCH=$GOARCH go build -a -ldflags "-X \"${GIT_COMMIT_LINK_FLAG}\" -X \"${GIT_TAGS_LINK_FLAG}\"" .
+mkdir -p build
+
+CGO_ENABLED=1 GOOS=linux GOARCH=$GOARCH go build -a -ldflags "-X \"${GIT_COMMIT_LINK_FLAG}\" -X \"${GIT_TAGS_LINK_FLAG}\"" -o build/bazel-remote .
